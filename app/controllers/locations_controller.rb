ce71f7f3f2d9531a8c9ca82f011c65d5ebe366af
redroboto <vince.pleyto@gmail.com>
2024-07-30 21:12:24 +0800

    feat: added navbar, sidebar, and working links

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,12 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.all
   end
+
+  def home
+    
+  end
+
+  def report
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'locations#index'
+  root 'locations#home'
+
+  get "/map", to: "locations#index"
+  get "/report", to: "locations#report"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
