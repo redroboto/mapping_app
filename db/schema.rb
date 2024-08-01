e8629d04d5462d8b63ad418c422ad3361f22b1db
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-08-01 14:22:50 +0200

    style: fixed layout and added responsiveness

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_29_131539) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_30_090316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.float "longitude"
+    t.float "latitude"
+    t.string "address"
+    t.boolean "is_2017"
+    t.boolean "is_2018"
+    t.boolean "is_2019"
+    t.boolean "is_2020"
+    t.boolean "is_2021"
+    t.boolean "is_2022"
+    t.boolean "is_2023"
+    t.boolean "is_2024"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
     t.string "name"
     t.float "longitude"
     t.float "latitude"
