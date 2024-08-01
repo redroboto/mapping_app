a7a0a711e9ed206d29164e4b8612b05ff7894eef
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-08-01 21:04:04 +0200

    feature: added added and displayed news api

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,10 +4,14 @@ class LocationsController < ApplicationController
   end
 
   def home
-    
+    api_key = 'cc38c00429dc4dee93ba2451d77f9ae5'
+    client = NewsService::Client.new(api_key)
+    result = client.fetch_pogo_news
+    @news = result[:data]['articles'] if result[:status] == 'Success'
+    @error_message = result[:data] unless result[:status] == 'Success'
   end
 
   def report
-    
+
   end
 end
