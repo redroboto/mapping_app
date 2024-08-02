5ad9540006621c3694709b28e85382f746f9bd2a
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-08-02 14:42:55 +0200

    fixed: conflict with manifest.js (javascript, application.css)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
-//= link_tree ../builds
 //= link style.css
-//= link_tree ../../javascript .js
 //= link swiper.js
+//= link_tree ../../javascript .js
+//= link application.css
