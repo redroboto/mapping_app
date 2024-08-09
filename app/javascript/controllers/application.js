94b44fcc8594583983c1fa20f47b8b32a8d7616b
redroboto <vince.pleyto@gmail.com>
2024-08-09 14:59:01 +0800

    fix: remove swiper code first to prevent conflicts with leaflet

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,49 +4,8 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
 
-    // Number of slides per view
-    slidesPerView: 1,
-
-    // Space between slides
-    spaceBetween: 20,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // Optional: responsive breakpoints
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      1280: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
-    },
-  });
